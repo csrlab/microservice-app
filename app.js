44181df7aacd69c4ae2a49720bccc34be1499939
sampson <sampsonoguntoye26@gmail.com>
2019-02-05 16:09:46 +0000

    usinf pattern for code reuse

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ seneca.add({role: 'math', cmd: 'sum', integer: true}, function (msg, respond) {
   var sum = Math.floor(msg.left) + Math.floor(msg.right)
   respond(null, {answer: sum})
 })
-*/
+
 var seneca = require('seneca')()
 
 seneca.add({role: 'math', cmd: 'sum'}, function (msg, respond) {
@@ -42,3 +42,27 @@ seneca.act({role: 'math', cmd: 'sum', left: 1.5, right: 2.5}, console.log)
 // BUT this matches role:math,cmd:sum,integer:true
 // because it's more specific - more properties match
 seneca.act({role: 'math', cmd: 'sum', left: 1.5, right: 2.5, integer: true}, console.log)
+*/
+
+var seneca = require('seneca')()
+
+seneca.add('role: math, cmd: sum', function (msg, respond) {
+  var sum = msg.left + msg.right
+  respond(null, {answer: sum})
+})
+
+seneca.add('role: math, cmd: sum, integer: true', function (msg, respond) {
+  // reuse role:math, cmd:sum
+  this.act({
+    role: 'math',
+    cmd: 'sum',
+    left: Math.floor(msg.left),
+    right: Math.floor(msg.right)
+  }, respond)
+})
+
+// this matches role:math,cmd:sum
+seneca.act('role: math, cmd: sum, left: 1.5, right: 2.5',console.log)
+
+// BUT this matches role:math,cmd:sum,integer:true
+seneca.act('role: math, cmd: sum, left: 1.5, right: 2.5, integer: true', console.log)
